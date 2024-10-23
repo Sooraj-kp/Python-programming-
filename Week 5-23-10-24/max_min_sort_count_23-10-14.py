b5a85ae1b167040fc0c55efcb7b52534c3656dac
Sooraj-kp <soorajkp2332@gmail.com>
2024-10-23 12:32:16 +0530

    Add files via upload

---
+++ Week 5-23-10-24/max_min_sort_count_23-10-14.py
@@ -0,0 +1,6 @@
+list=[13,4,5,6,7,7,9,8]
+print("min:",min(list))
+print("max:",max(list))
+print("count:",list.count(7))
+print("sort:",sorted(list))
+print("reverse sort:",sorted(list,reverse=True))
